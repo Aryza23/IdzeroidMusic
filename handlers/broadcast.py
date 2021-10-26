b539a6d4e13b4e647e6a4ebac286abe1ef5363fa
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-10-26 17:00:07 +0700

    Add files via upload

---
+++ handlers/broadcast.py
@@ -0,0 +1,33 @@
+# Copyright (C) 2021 By KennedyProject
+
+
+import asyncio
+
+from pyrogram import Client, filters
+from pyrogram.types import Dialog, Chat, Message
+from pyrogram.errors import UserAlreadyParticipant
+
+from callsmusic.callsmusic import client as kennedy
+from config import SUDO_USERS
+
+@Client.on_message(filters.command(["gmcast"]))
+async def broadcast(_, message: Message):
+    sent=0
+    failed=0
+    if message.from_user.id not in SUDO_USERS:
+        return
+    else:
+        wtf = await message.reply("`Starting Broadcast...`")
+        if not message.reply_to_message:
+            await wtf.edit("Please Reply To A Message To Start Broadcast !")
+            return
+        lmao = message.reply_to_message.text
+        async for dialog in kennedy.iter_dialogs():
+            try:
+                await kennedy.send_message(dialog.chat.id, lmao)
+                sent = sent+1
+                await wtf.edit(f"`Broadcasting...` \n\n**Sent To :** `{sent}` Chats \n**Failed In :** {failed} Chats")
+                await asyncio.sleep(3)
+            except:
+                failed=failed+1
+        await message.reply_text(f"`Gcast Succesfully` \n\n**Sent To :** `{sent}` Chats \n**Failed In :** {failed} Chats")
